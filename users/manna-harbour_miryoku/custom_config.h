54cfd5e693a1adf94573bb77ed00c139b5d36d5b
Johannes <jhn@kth.se>
2024-11-04 21:52:21 +0100

    update naming

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -5,7 +5,7 @@
 
 #pragma once
 
-#define MIRYOKU__SYM \
+#define MIRYOKU_LAYER_SYM \
 KC_TILD,           KC_GRV,            KC_QUOT,           KC_DQUO,           KC_LPRN,           KC_RPRN,           KC_PIPE,           KC_BSLS,           U_NA,              U_NA,              \
 KC_CIRC,           KC_EQL,            KC_UNDS,           KC_DLR,            KC_LCBR,           KC_RCBR,           KC_EXLM,           KC_LT,             KC_GT,             U_NA,              \
 KC_HASH,           KC_AMPR,           KC_QUES,           KC_ASTR,           KC_LBRC,           KC_RBRC,           KC_PLUS,           KC_MINS,           KC_AT,             U_NA,              \
